f43727c460c62f5678baf5f4b38713fbc2107939
JP <dragonginger10@gmail.com>
2024-02-26 15:33:19 -0600

    flipper builds

--- pkgs/hypixelFlipper/default.nix
+++ pkgs/hypixelFlipper/default.nix
@@ -9,23 +9,23 @@
 }: let
   pname = "hypixelFlipper";
   version = "1.0";
-  in
-buildPythonApplication {
-  inherit pname version;
+in
+  buildPythonApplication {
+    inherit pname version;
+    format = "pyproject";
 
-  src = fetchFromGitHub{
-    owner = "dragonginger10";
-    repo = pname;
-    rev = "main";
-    hash = "";
-  };
+    src = fetchFromGitHub {
+      owner = "dragonginger10";
+      repo = pname;
+      rev = "main";
+      hash = "sha256-NLMlWrGJzaMd0d3hm5ZgjF18JuVSl05pteYajAAoER8=";
+    };
 
-  nativeBuildInputs = [setuptools];
-  propagatedBuildInputs = [
-    requests
-    pandas
-    rich
-    typer
-  ];
-  
-}
+    nativeBuildInputs = [setuptools];
+    propagatedBuildInputs = [
+      requests
+      pandas
+      rich
+      typer
+    ];
+  }
