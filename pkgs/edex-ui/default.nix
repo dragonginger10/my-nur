d9f32fa9e91c1ef7982b2f49eb41273ddab93c7a
JP <dragonginger10@gmail.com>
2024-02-26 08:48:13 -0600

    version for edex

--- pkgs/edex-ui/default.nix
+++ pkgs/edex-ui/default.nix
@@ -3,11 +3,13 @@
   fetchurl,
 }: let
   pname = "edex-ui";
+  version = "2.2.8";
 in
   appimageTools.wrapType2 {
+    inherit version;
     name = pname;
     src = fetchurl {
-      url = "https://github.com/GitSquared/edex-ui/releases/download/v2.2.8/eDEX-UI-Linux-x86_64.AppImage";
+      url = "https://github.com/GitSquared/edex-ui/releases/download/v${version}/eDEX-UI-Linux-x86_64.AppImage";
       sha256 = "10fn0as77ikrpfpjxhs3lr0w8kg6ldn7a2wnq6h2q0ya47bqrwn8";
     };
   }
