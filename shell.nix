2f6f22ba43f97a57bd2f1839c4c071812e66b2e0
JP <dragonginger10@gmail.com>
2024-02-28 16:06:07 -0600

    remove unneeded packages

--- shell.nix
+++ shell.nix
@@ -4,13 +4,6 @@
     NIX_CONFIG = "experimental-features = nix-command flakes";
     nativeBuildInputs = with pkgs; [
       nix
-      just
-      git
-      home-manager
-      nil
-      taplo
-      lua-language-server
-      nodePackages.bash-language-server
       statix
       nix-init
     ];
