456eb2560d6156814eddae14505b4b7e2dba2204
JP <dragonginger10@gmail.com>
2024-02-28 16:38:34 -0600

    add pre-commit for formatting

--- flake.nix
+++ flake.nix
@@ -1,9 +1,15 @@
 {
   description = "My personal NUR repository";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+    };
+  };
   outputs = {
     self,
     nixpkgs,
+    pre-commit-hooks,
   }: let
     systems = [
       "x86_64-linux"
@@ -15,11 +21,20 @@
     ];
     forAllSystems = f: nixpkgs.lib.genAttrs systems f;
   in {
+    checks = forAllSystems (system: {
+      pre-commit-check = pre-commit-hooks.lib.${system}.run {
+        src = ./.;
+        hooks = {
+          alejandra.enable = true;
+        };
+      };
+    });
     formatter = forAllSystems (system: let pkgs = import nixpkgs {inherit system;}; in pkgs.alejandra);
     devShells = forAllSystems (system: let
+      inherit (self.checks.${system}.pre-commit-check) shellHook;
       pkgs = import nixpkgs {inherit system;};
     in
-      import ./shell.nix {inherit pkgs;});
+      import ./shell.nix {inherit pkgs shellHook;});
 
     legacyPackages = forAllSystems (system:
       import ./default.nix {
